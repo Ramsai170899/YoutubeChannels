ceeeed1d5c571ed692de12f872e1d4997f8f0003
Ramsai Jagadish <81312213+Ramsai170899@users.noreply.github.com>
2025-03-17 16:06:48 +0530

    Create tts.py

---
+++ code/basic/tts.py
@@ -0,0 +1,62 @@
+# pip install edge-tts nest_asyncio
+
+
+import edge_tts
+import asyncio
+import IPython.display as ipd
+import nest_asyncio
+import pandas as pd
+import re
+
+nest_asyncio.apply()
+
+
+async def get_voices_df():
+    voices = await edge_tts.list_voices()
+    df = pd.DataFrame(voices)
+    return df
+
+voices_df = asyncio.run(get_voices_df())
+
+voices_df['FriendlyName'] = voices_df['FriendlyName'].apply(
+    lambda x: re.sub(r'Microsoft|Online|Natural|(|)|\(\)', '', x).strip()
+)
+
+voices_list = (
+    voices_df[voices_df['Gender'] == 'Female'][['ShortName', 'FriendlyName']]
+    .loc[lambda x: x['FriendlyName'].str.contains(r'English', regex=True)]
+    .assign(FriendlyName=lambda x: x['FriendlyName'].str.replace(r'\s*\(\)', '', regex=True))
+    .apply(tuple, axis=1)
+    .tolist()
+)
+
+text = """
+
+Alright, listen to me. *Listen*. You want to be successful. That's a whisper in the dark, a universal yearning. 
+
+"""
+
+
+
+person = "AvaMultilingualNeural"
+location = "US"
+filename = """success"""
+
+
+rate = "+5%"
+pitch = "-5Hz"
+
+
+async def text_to_speech(text, filename="output.mp3", voice=f"en-{location}-{person}", rate=rate, pitch=pitch):
+    text = re.sub(r"[*/#]", " ", text).replace("—", "; ")
+    tts = edge_tts.Communicate(text, voice=voice, rate=rate, pitch=pitch)
+    audio_path = f"{filename}_{person}_{rate}_{pitch}.mp3"
+    await tts.save(audio_path)
+    return text, audio_path, ipd.Audio(audio_path, autoplay=True)
+refined_text, filename,audio_output = asyncio.run(text_to_speech(text, filename=filename))
+
+print(f"Audio file is generated: {filename}")
+
+
+
+
